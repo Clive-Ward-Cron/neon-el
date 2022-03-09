6d02c366fdae3ebba0cf2858054b4835e28d734d
Clive-Ward-Cron <clive_wardcron@live.com>
2022-03-09 09:28:13 -0700

    Solved issue with clipped words from text nodes being wrapped in spans being converted to SVG, domtoimage was applying a white-space style that wasn't applied to the actual element to allow word wrapping, had to explicitly set it to nowrap in default styles

--- handlers.js
+++ handlers.js
@@ -5,20 +5,25 @@ import { getComputedStyleObject, wrapIfTextNode } from "./utils";
 export function makeImage(e) {
   // console.log(e);
   const el = wrapIfTextNode.bind(this)(e.target.assignedNodes()[0]);
+  // const el = e.target.assignedNodes()[0];
 
   const rect = el?.getBoundingClientRect ? el.getBoundingClientRect() : this.getBoundingClientRect();
+
+  console.groupCollapsed(`makeImage Handler ${this.neonId}`);
   console.log(this.neonId, rect);
-  console.log(this);
-  // console.dir(el);
+  // console.log(this);
+  console.dir(el);
   // console.dir(this);
   // console.log(this.neonId);
   // console.table(getComputedStyleObject(el));
+  console.groupEnd();
 
   // Make an image out of the slotted node and assign it as the background image
   if (this.shadowRoot.querySelector("slot").assignedNodes().length > 0) {
     const overwrite = {};
     overwrite["margin-block"] = "0";
     overwrite["margin"] = "0";
+    overwrite["white-space"] = "nowrap"; // Text nodes in spans were wrapping for no apparent reason
     console.log(rect.width, rect.height);
     domtoimage
       .toSvg(el, {
@@ -27,13 +32,15 @@ export function makeImage(e) {
         style: Object.assign(getComputedStyleObject(el), overwrite),
       })
       .then((dataURL) => {
-        console.log(this);
         this.src = dataURL;
         this.blurAmt = "10";
         this.width = rect.width + "px";
         this.height = rect.height + "px";
         el.style.display = "none";
+        console.groupCollapsed(`SVG data URL ${this.neonId}`);
+        console.log(this);
         console.log(dataURL);
+        console.groupEnd();
       });
   }
 }
